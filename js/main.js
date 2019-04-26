45b007bc21042b4ad65e77ae7ed01cb89c7fefbf
Sahitya Maruvada <sahitya.lalitha@gmail.com>
2019-04-25 20:51:57 -0700

    Initial commit adding html,CSS and js

---
+++ js/main.js
@@ -0,0 +1,32 @@
+(function() {
+	function toJSONString( form ) {
+		var obj = {};
+		var elements = form.querySelectorAll( "input, select" );
+		for( var i = 0; i < elements.length; ++i ) {
+			var element = elements[i];
+			var name = element.name;
+			var value = element.value;
+
+			if( name ) {
+				obj[ name ] = value;
+			}
+		}
+
+		return JSON.stringify( obj );
+	}
+
+	document.addEventListener( "DOMContentLoaded", function() {
+		var form = document.getElementById( "test" );
+		var output = document.getElementById( "output" );
+		form.addEventListener( "submit", function( e ) {
+			e.preventDefault();
+			var json = toJSONString( this );
+			output.innerHTML = json;
+
+			console.log(json)
+
+		}, false);
+
+	});
+
+})();
